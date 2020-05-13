07230a1072d5f6fa8aad254d8c97f60bdc494731
Marciel Barcellos <msbar2@gmail.com>
2020-05-12 23:29:07 -0300

    initial config

--- djangojwt/settings.py
+++ djangojwt/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'robustmanager.urls'
+ROOT_URLCONF = 'djangojwt.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'robustmanager.wsgi.application'
+WSGI_APPLICATION = 'djangojwt.wsgi.application'
 
 
 # Database
